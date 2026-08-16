f44504049c93541fc20d7ecfaed7fb53c970069f
supdger <supdger@163.com>
2026-08-16 19:32:07 +0800

    feat: migrate SandAdmin identity (17)
    AI-Implemented: OpenAI Codex

---
+++ sandadmin-artd/src/components/core/base/art-logo/index.vue
@@ -0,0 +1,26 @@
+<!-- 系统logo -->
+<template>
+  <div class="flex-cc">
+    <img
+      :style="logoStyle"
+      src="@imgs/common/sandadmin-mark-transparent.png"
+      alt="SandAdmin"
+      class="w-full h-full"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineOptions({ name: 'ArtLogo' })
+
+  interface Props {
+    /** logo 大小 */
+    size?: number | string
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    size: 36
+  })
+
+  const logoStyle = computed(() => ({ width: `${props.size}px` }))
+</script>
