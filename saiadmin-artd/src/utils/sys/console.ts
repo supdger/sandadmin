39dd44974fa4ca79ac7488399a66397f3120638e
supdger <supdger@163.com>
2026-08-16 19:32:06 +0800

    feat: migrate SandAdmin identity (10)
    AI-Implemented: OpenAI Codex

--- saiadmin-artd/src/utils/sys/console.ts
+++
@@ -1,8 +0,0 @@
-// ANSI 转义码生成网站  https://patorjk.com/software/taag/#p=display&f=Big&t=ABB%0A
-const asciiArt = `
-\x1b[32m欢迎使用 SaiAdmin 6.x
-\x1b[0m
-\x1b[36mSaiAdmin 官网: https://saithink.top
-\x1b[0m
-`
-console.log(asciiArt)
